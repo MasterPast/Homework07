81b6afd87fbc3a3255bb690b17e11231d107a656
MasterPast <igory733@gmail.com>
2023-08-27 21:48:09 +0300

    last

--- temp.py
+++ temp.py
@@ -1,28 +1,31 @@
-data = [1, [2, 3], [4, [5, [6, 7]]], [[[8], 9], [10]]]
+data = ['X', 3, 'Z', 2, 'X', 2, 'Y', 3, 'Z', 2]
+     #   0   1   2   3   4
 
-def flatten(data):
-    st = []
-    print(data)
-    for ind, lst in enumerate(data):
-        if type(lst) == type(data):
-            data += flatten(lst)
-            
-            print(data)
-        # else:
-            # data += flatten(lst)
-    for ind, lst in enumerate(data):
-        if type(lst) == type(data):
-            data.remove(data[ind])
-    # data.append(int(st))
+def decode(encoded_list):
+    # print(type(encoded_list[0]))
+    # print(type(encoded_list[1]))
+    # print(isinstance(encoded_list[0], int))
+    # print(isinstance(encoded_list[1], int))
+
     
-    # print(data)
-    return data
+    if not encoded_list:
+        return []
 
-flatten(data)
-print(f'itogo: {data}')
-# c=[]
-# a=[1,3,6]
-# b=[2,4,8]
-# # a.insert(2,b)
-# c=a+b
-# print(c)
+    head = encoded_list[0]
+    print(f'head>>> {head}')
+    if isinstance(head, int):
+        print(head)
+        print(type(head))
+        if len(encoded_list) >= 2:
+            value = encoded_list[1]
+            rest_of_list = encoded_list[2:]
+            decoded_part = [value] * head
+            print(decoded_part)
+            return decoded_part + decode(rest_of_list)
+        else:
+            return []
+    else:
+        rest_of_list = encoded_list[1:]
+        return [head] + decode(rest_of_list)
+re = decode(data)
+print(re)
---
+++ temp2.py
@@ -0,0 +1,7 @@
+data = ['X', 3, 'Z', 2, 'X', 2, 'Y', 3, 'Z', 2]
+a = 10
+b = 'ssdfsd'
+print(type(data[0]))
+print(type(data[1]))
+print(isinstance(data[0], int))
+print(isinstance(data[1], int))
